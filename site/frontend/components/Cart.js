255eaff772b07c246ef5db6c55f8bf2b7d1f38ac
Jake Acosta <jakeacosta@Jakes-MBP.attlocal.net>
2020-04-29 16:39:41 -0700

    makes nested mutations and queries more readable

--- site/frontend/components/Cart.js
+++ site/frontend/components/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { adopt } from 'react-adopt';
 import { Mutation, Query } from 'react-apollo';
 
 import { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION } from '../lib/queries';
@@ -11,45 +12,44 @@ import CloseButton from './styles/CloseButton';
 import Supreme from './styles/Supreme';
 import User from './User';
 
-const Cart = () => {
-	return (
-		<User>
-			{({ data: { me } }) => {
-				if (!me) return null;
-				return (
-					<Mutation mutation={TOGGLE_CART_MUTATION}>
-						{(toggleCart) => (
-							<Query query={LOCAL_STATE_QUERY}>
-								{({ data }) => (
-									<CartStyles open={data.cartOpen}>
-										<header>
-											<CloseButton title='close' onClick={toggleCart}>
-												&times;
-											</CloseButton>
-											<Supreme>{me.name}'s Cart</Supreme>
-											<p>
-												{me.cart.length} item{me.cart.length !== 1 ? 's' : null}{' '}
-												in your cart
-											</p>
-										</header>
-										<ul>
-											{me.cart.map((cartItem) => (
-												<CartItem key={cartItem.id} cartItem={cartItem} />
-											))}
-										</ul>
-										<footer>
-											<p>${calcTotalPrice(me.cart)}</p>
-											<Button>Checkout</Button>
-										</footer>
-									</CartStyles>
-								)}
-							</Query>
-						)}
-					</Mutation>
-				);
-			}}
-		</User>
-	);
-};
+const Composed = adopt({
+	user: ({ render }) => <User>{render}</User>,
+	toggleCart: ({ render }) => (
+		<Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>
+	),
+	localState: ({ render }) => <Query query={LOCAL_STATE_QUERY}>{render}</Query>,
+});
+
+const Cart = () => (
+	<Composed>
+		{({ user, toggleCart, localState }) => {
+			const me = user.data.me;
+			if (!me) return null;
+			return (
+				<CartStyles open={localState.data.cartOpen}>
+					<header>
+						<CloseButton title='close' onClick={toggleCart}>
+							&times;
+						</CloseButton>
+						<Supreme>{me.name}'s Cart</Supreme>
+						<p>
+							{me.cart.length} item{me.cart.length !== 1 ? 's' : null} in your
+							cart
+						</p>
+					</header>
+					<ul>
+						{me.cart.map((cartItem) => (
+							<CartItem key={cartItem.id} cartItem={cartItem} />
+						))}
+					</ul>
+					<footer>
+						<p>${calcTotalPrice(me.cart)}</p>
+						<Button>Checkout</Button>
+					</footer>
+				</CartStyles>
+			);
+		}}
+	</Composed>
+);
 
 export default Cart;
--- site/frontend/components/Nav.js
+++ site/frontend/components/Nav.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { adopt } from 'react-adopt';
 import { Mutation } from 'react-apollo';
 
 import CartCount from './CartCount';
@@ -8,51 +9,57 @@ import User from './User';
 
 import { TOGGLE_CART_MUTATION } from '../lib/queries';
 
+const Composed = adopt({
+	user: ({ render }) => <User>{render}</User>,
+	toggleCart: ({ render }) => (
+		<Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>
+	),
+});
+
 const Nav = () => (
-	<User>
-		{({ data: { me } }) => (
-			<>
-				<NavStyles>
-					<div>{`Logged in as ${me.name}`}</div>
-					<Link href='/store'>
-						<a>Store</a>
-					</Link>
-					{me && (
-						<>
-							<Link href='/sell'>
-								<a>Sell</a>
-							</Link>
-							<Link href='/signup'>
-								<a>Signup</a>
-							</Link>
-							<Link href='/orders'>
-								<a>Orders</a>
-							</Link>
-							<Signout />
-							<Mutation mutation={TOGGLE_CART_MUTATION}>
-								{(toggleCart) => (
-									<button onClick={toggleCart}>
-										My Cart
-										<CartCount
-											count={me.cart.reduce(
-												(total, item) => total + item.quantity,
-												0
-											)}
-										/>
-									</button>
-								)}
-							</Mutation>
-						</>
-					)}
-					{!me && (
-						<Link href='/signup'>
-							<a>Log in / Sign Up</a>
+	<Composed>
+		{({ user, toggleCart, localState }) => {
+			const me = user.data.me;
+			return (
+				<>
+					<NavStyles>
+						<div>{`Logged in as ${me && me.name}`}</div>
+						<Link href='/store'>
+							<a>Store</a>
 						</Link>
-					)}
-				</NavStyles>
-			</>
-		)}
-	</User>
+						{me && (
+							<>
+								<Link href='/sell'>
+									<a>Sell</a>
+								</Link>
+								<Link href='/signup'>
+									<a>Signup</a>
+								</Link>
+								<Link href='/orders'>
+									<a>Orders</a>
+								</Link>
+								<Signout />
+								<button onClick={toggleCart}>
+									My Cart
+									<CartCount
+										count={me.cart.reduce(
+											(total, item) => total + item.quantity,
+											0
+										)}
+									/>
+								</button>
+							</>
+						)}
+						{!me && (
+							<Link href='/signup'>
+								<a>Log in / Sign Up</a>
+							</Link>
+						)}
+					</NavStyles>
+				</>
+			);
+		}}
+	</Composed>
 );
 
 export default Nav;
